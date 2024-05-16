332e5eaf14d3c81e6ff319310567123d9f221226
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-05-16 11:44:27 -0400

    first

---
+++ MortgageApp/MortgageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortgageApp
+//
+//  Created by Tiparpron Sukanya on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
